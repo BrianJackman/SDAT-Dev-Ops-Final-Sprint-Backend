b426eb2f71e0dc29791bfb8eb346d1c6f16c0d4d
Brian Jackman <brian.jackman@keyin.com>
2025-04-17 12:21:01 -0230

    Set up initial Spring Boot project

---
+++ SDAT-Dev-Ops-Final-Sprint-Backend/src/main/java/com/keyin/SDAT_Dev_Ops_Final_Sprint_Backend/SdatDevOpsFinalSprintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.SDAT_Dev_Ops_Final_Sprint_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdatDevOpsFinalSprintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdatDevOpsFinalSprintBackendApplication.class, args);
+	}
+
+}
---
+++ SDAT-Dev-Ops-Final-Sprint-Backend/src/test/java/com/keyin/SDAT_Dev_Ops_Final_Sprint_Backend/SdatDevOpsFinalSprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.SDAT_Dev_Ops_Final_Sprint_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdatDevOpsFinalSprintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
